21d5441ac11c7083558d6d4e4dbbe2171999e6ba
momoandbanana <momo_and_banana@yahoo.co.jp>
2025-08-12 18:49:19 +0900

    Initial commit: Project setup with .NET 8.0 and xUnit
    - Created console application project
    - Added xUnit test project
    - Configured solution structure
    - Added .gitignore for .NET projects

---
+++ VRChatDateTimeFixer.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Xunit;
+
+namespace VRChatDateTimeFixer.Tests
+{
+    public class DateTimeFixerTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            // テストコードをここに記述
+            Assert.True(true);
+        }
+    }
+}
