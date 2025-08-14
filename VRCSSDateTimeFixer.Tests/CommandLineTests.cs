0fdd42740cb8c140669a7c517e13f58b58c8e1b0
momoandbanana <momo_and_banana@yahoo.co.jp>
2025-08-14 13:14:36 +0900

    test: コマンドラインテスト

--- VRCSSDateTimeFixer.Tests/CommandLineTests.cs
+++ VRCSSDateTimeFixer.Tests/CommandLineTests.cs
@@ -1,7 +1,9 @@
 using System.CommandLine;
 using System.CommandLine.Parsing;
+using System.IO;
 using VRCSSDateTimeFixer;
 using Xunit;
+using System.Linq;
 
 namespace VRCSSDateTimeFixer.Tests
 {
@@ -38,16 +40,100 @@ namespace VRCSSDateTimeFixer.Tests
         }
 
         [Fact]
-        public void ヘルプオプションが指定された場合_ヘルプを表示する()
+        public void ヘルプオプションが指定された場合_パースが成功する()
         {
             // Arrange
             string[] args = new[] { "--help" };
             
             // Act
-            var parseResult = Program.BuildCommandLine().Parse(args);
+            var command = Program.BuildCommandLine();
+            var parseResult = command.Parse(args);
             
             // Assert
             Assert.Empty(parseResult.Errors);
+            // ヘルプ表示がリクエストされたことを確認
+            Assert.True(parseResult.Errors.Count == 0);
+        }
+
+        [Fact]
+        public void 必須引数が指定されていない場合_エラーを返す()
+        {
+            // Arrange
+            string[] args = Array.Empty<string>();
+            
+            // Act
+            var command = Program.BuildCommandLine();
+            var parseResult = command.Parse(args);
+            
+            // Assert
+            Assert.NotEmpty(parseResult.Errors);
+            Assert.Contains("Required argument missing for command", parseResult.Errors[0].Message);
+        }
+
+        [Fact]
+        public void 存在しないファイルが指定された場合_エラーを返す()
+        {
+            // Arrange
+            string nonExistentFile = Path.Combine(Path.GetTempPath(), "nonexistent_file.png");
+            string[] args = new[] { nonExistentFile };
+            
+            // Act
+            var parseResult = Program.BuildCommandLine().Parse(args);
+            
+            // Assert
+            Assert.Empty(parseResult.Errors); // パース時点ではエラーにならない
+            
+            // 実際の処理時にエラーになることを確認するためのテストも可能
+            // ここではパースのテストに留める
+        }
+
+        [Fact]
+        public void 無効なオプションが指定された場合_エラーを返す()
+        {
+            // Arrange
+            string[] args = new[] { "valid.png", "--invalid-option" };
+            
+            // Act
+            var command = Program.BuildCommandLine();
+            var parseResult = command.Parse(args);
+            
+            // Assert
+            Assert.NotEmpty(parseResult.Errors);
+            Assert.Contains("Unrecognized command or argument", parseResult.Errors[0].Message);
+        }
+
+        [Fact]
+        public void 再帰オプションが指定されていない場合_デフォルトでfalseとなること()
+        {
+            // Arrange
+            string[] args = new[] { "C:\\path\\to\\directory" };
+            
+            // Act
+            var command = Program.BuildCommandLine();
+            var parseResult = command.Parse(args);
+            
+            // Assert
+            Assert.Empty(parseResult.Errors);
+            var recursiveOption = (Option<bool>)command.Options.First(o => o.Name == "recursive");
+            Assert.False(parseResult.GetValueForOption(recursiveOption));
+        }
+
+        [Theory]
+        [InlineData("-r")]
+        [InlineData("--recursive")]
+        public void 再帰オプションのエイリアスが正しく動作すること(string option)
+        {
+            // Arrange
+            string[] args = new[] { "C:\\path\\to\\directory", option };
+            
+            // Act
+            var command = Program.BuildCommandLine();
+            var parseResult = command.Parse(args);
+            
+            // Assert
+            Assert.Empty(parseResult.Errors);
+            var recursiveOption = (Option<bool>)command.Options.First(o => o.Name == "recursive");
+            Assert.True(parseResult.GetValueForOption(recursiveOption));
         }
 
         [Fact]
